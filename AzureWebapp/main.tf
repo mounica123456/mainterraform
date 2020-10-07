80949c973272b1571f754ad4c52674c0c1f5d9a0
mounica123456 <72482632+mounica123456@users.noreply.github.com>
2020-10-07 19:10:31 +0530

    Update main.tf

--- AzureWebapp/main.tf
+++ AzureWebapp/main.tf
@@ -1,10 +1,10 @@
 # Configure the Azure provider
 provider "azurerm" {
   version         = "=2.4.0"
-  subscription_id = "2489a3c7-9e54-4074-a38d-eb3af3bdc14f"
-  client_id       = "598fe56e-920e-428d-b16a-4b9386497ce3"
-  client_secret   = "jL6hqfkrx63b5N-2.E0S5h0.FFpu.5Pp4x"
-  tenant_id       = "c0447ee7-7b4a-40e6-85b3-dc6eda2714de"
+  subscription_id = "88b7a74c-47d4-48ed-b11f-9bac8f99264c"
+  client_id       = "4fe13f05-87c0-4e6e-8e33-3df885a7704e"
+  client_secret   = "4d_LfSPBooQdNN~6D8.ysrNpv3zWs-kXP0"
+  tenant_id       = "70973106-6728-471c-8ec4-08f6fa9e70bd"
   features {}
 }
 resource "azurerm_resource_group" "webrg2" {
